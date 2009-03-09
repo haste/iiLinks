d2e77fc75839b540463ee92ce98cb0318693d6a8
Trond A Ekseth <troeks@gmail.com>
2009-03-09 18:28:00 +0100

    Make it work correctly on 3.0 and play nice with other add-ons.

--- links.lua
+++ links.lua
@@ -1,57 +1,35 @@
---[[-------------------------------------------------------------------------
-  Copyright (c) 2007, Trond A Ekseth
-  All rights reserved.
+local frame = CreateFrame"Frame"
+frame:RegisterEvent'PLAYER_LOGIN'
+frame:SetScript('OnEvent', function()
+	local origs = {}
 
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are
-  met:
+	local type = type
+	local _Insert = ChatFrameEditBox.Insert
 
-      * Redistributions of source code must retain the above copyright
-        notice, this list of conditions and the following disclaimer.
-      * Redistributions in binary form must reproduce the above
-        copyright notice, this list of conditions and the following
-        disclaimer in the documentation and/or other materials provided
-        with the distribution.
-      * Neither the name of iiLinks nor the names of its contributors may
-        be used to endorse or promote products derived from this
-        software without specific prior written permission.
+	local AddMessage = function(self, text,...)
+		if(type(text) == "string") then
+			text = text:gsub("|H(.-)|h%[(.-)%]|h", "|H%1|h%2|h")
+		end
 
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
----------------------------------------------------------------------------]]
-
-local type = type
-local _AddMessage = ChatFrame1.AddMessage
-local _Insert = ChatFrameEditBox.Insert
-
-local AddMessage = function(self, text,...)
-	if(type(text) == "string") then
-		text = text:gsub("|H(.-)|h%[(.-)%]|h", "|H%1|h%2|h")
+		return origs[self](self, text, ...)
 	end
 
-	return _AddMessage(self, text, ...)
-end
+	_G.ChatFrameEditBox.Insert = function(self, str, ...)
+		if(type(str) == "string") then
+			str = str:gsub("|H(.-)|h[%[]?(.-)[%]]?|h", "|H%1|h[%2]|h")
+		end
 
-_G.ChatFrameEditBox.Insert = function(self, str, ...)
-	if(type(str) == "string") then
-		str = str:gsub("|H(.-)|h[%[]?(.-)[%]]?|h", "|H%1|h[%2]|h")
+		return _Insert(self, str, ...)
 	end
 
-	return _Insert(self, str, ...)
-end
-
-for i=1, NUM_CHAT_WINDOWS do
-	if(i ~= 2) then
-		local cf = _G["ChatFrame"..i]
-		cf.AddMessage = AddMessage
+	for i=1, NUM_CHAT_WINDOWS do
+		if(i ~= 2) then
+			local cf = _G["ChatFrame"..i]
+			origs[cf] = cf.AddMessage
+			cf.AddMessage = AddMessage
+		end
 	end
-end
+
+	frame:SetScript('OnEvent', nil)
+	frame:UnregisterEvent'PLAYER_LOGIN'
+end)
